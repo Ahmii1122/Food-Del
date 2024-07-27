21fb8220d7157efae488aaf7aaa10f0a804a4683
ahmii1122 <asam4055144@gmail.com>
2024-07-27 23:21:57 +0500

    code refactor

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,32 +1,41 @@
-import { useContext, useState } from 'react'
-import { assets } from '../../assets/assets'
-import './Navbar.css'
-import { Link } from 'react-router-dom'
-import { StoreContext } from '../../Context/StoreContext'
-interface Props{
-    setshowlogin:()=>void
-}
-const Navbar = ({setshowlogin}:Props) => {
-    const[menu,setmenu]=useState('home');
-    const {gettotalcartamount}=useContext(StoreContext);
-  return (
-    <div className='navbar'>
-        <Link to='/'><img src={assets.logo} alt="logo" className="logo" /></Link>
-        <ul className='navbar-menu'>
-            <Link to='/' onClick={()=> setmenu('home')} className={menu === "home"?"active":""}>Home</Link>
-            <a href='#explore-menu' onClick={()=> setmenu('menu')} className={menu === "menu"? "active":""}> Menu</a>
-            <a href='#footer' onClick={()=> setmenu('contact-us')} className={menu === "contact-us"? "active":""}>Contact Us</a>
-        </ul>
-        <div className="nabar-right">
-            <img src={assets.search_icon} alt="" />
-            <div className="navbar-search-icon">
-                <Link to='/cart'><img  src={assets.basket_icon} alt="cart icon" /></Link>
-                <div className={gettotalcartamount()===0?"":"dot"}></div>
-            </div>
-            <button onClick={()=>setshowlogin()}>Sign In</button>
-        </div>
-    </div>
-  )
+import { useContext, useState } from 'react';
+import { assets } from '../../assets/assets';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+import { StoreContext } from '../../Context/StoreContext';
+
+interface Props {
+  setshowlogin: () => void;
 }
 
-export default Navbar
+const Navbar = ({ setshowlogin }: Props) => {
+  const [menu, setmenu] = useState('home');
+  const storeContext = useContext(StoreContext);
+
+  if (!storeContext) {
+    throw new Error('StoreContext must be used within a StoreProvider');
+  }
+
+  const { gettotalcartamount } = storeContext;
+
+  return (
+    <div className='navbar'>
+      <Link to='/'><img src={assets.logo} alt="logo" className="logo" /></Link>
+      <ul className='navbar-menu'>
+        <Link to='/' onClick={() => setmenu('home')} className={menu === "home" ? "active" : ""}>Home</Link>
+        <a href='#explore-menu' onClick={() => setmenu('menu')} className={menu === "menu" ? "active" : ""}>Menu</a>
+        <a href='#footer' onClick={() => setmenu('contact-us')} className={menu === "contact-us" ? "active" : ""}>Contact Us</a>
+      </ul>
+      <div className="nabar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <Link to='/cart'><img src={assets.basket_icon} alt="cart icon" /></Link>
+          <div className={gettotalcartamount() === 0 ? "" : "dot"}></div>
+        </div>
+        <button onClick={() => setshowlogin()}>Sign In</button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
--- frontend/src/pages/Placeorder/Placeorder.tsx
+++ frontend/src/pages/Placeorder/Placeorder.tsx
@@ -1,10 +1,18 @@
-import { useContext } from 'react'
-import './Placeorder.css'
-import { StoreContext } from '../../Context/StoreContext'
+import { useContext } from 'react';
+import './Placeorder.css';
+import { StoreContext } from '../../Context/StoreContext';
+
 const Placeorder = () => {
-  const {gettotalcartamount} = useContext(StoreContext);
+  const storeContext = useContext(StoreContext);
+
+  if (!storeContext) {
+    throw new Error('StoreContext must be used within a StoreProvider');
+  }
+
+  const { gettotalcartamount } = storeContext;
+
   return (
-    <form className='place-order' >
+    <form className='place-order'>
       <div className="place-order-left">
         <p className='title'>Delivery Information</p>
         <div className="multi-fileds">
@@ -12,7 +20,7 @@ const Placeorder = () => {
           <input type="text" placeholder='Last Name' />
         </div>
         <input type="email" placeholder='Email' />
-        <input type="text" placeholder='Street'/>
+        <input type="text" placeholder='Street' />
         <div className="multi-fileds">
           <input type="text" placeholder='City' />
           <input type="text" placeholder='State' />
@@ -25,33 +33,29 @@ const Placeorder = () => {
       </div>
       
       <div className="place-order-right">
-         <div className="cart-total">
+        <div className="cart-total">
           <h2>Cart Total</h2>
           <div>
             <div className="cart-total-details">
               <p>SubTotal</p>
               <p>${gettotalcartamount()}</p>
-              
             </div>
             <hr />
             <div className="cart-total-details">
-          <p>Delivery Fee</p>
-          <p>${gettotalcartamount()===0?0:2}</p>
-          
+              <p>Delivery Fee</p>
+              <p>${gettotalcartamount() === 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
-            <b>Total</b>
-            <b>${gettotalcartamount()===0?0:gettotalcartamount()+2}</b>
+              <b>Total</b>
+              <b>${gettotalcartamount() === 0 ? 0 : gettotalcartamount() + 2}</b>
             </div>
-            
           </div>
           <button>Proceed To Payment</button>
         </div>
       </div>
-
     </form>
-  )
-}
+  );
+};
 
-export default Placeorder
+export default Placeorder;
