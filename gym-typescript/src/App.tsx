a3881ee309ee232b9d7e60d91c57a0caf294b684
ahmii1122 <asam4055144@gmail.com>
2024-08-03 17:33:19 +0500

    complete home and navbar

--- gym-typescript/src/App.tsx
+++ gym-typescript/src/App.tsx
@@ -1,11 +1,33 @@
 import Navbar from "@/scenes/Navbar"
+import Home from "@/scenes/Home"
+import { useEffect, useState } from "react"
+import { SelectedPage } from "@/Shared/types"
+
 
 function App() {
+  const [selectedPage,setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [isTopOfPage,setIsTopOfPage]= useState<boolean>(true);
+  useEffect(()=>{
+    const handlescroll = ()=>{
+      if(window.scrollY === 0)
+      {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if(window.scrollY !== 0)
+      {
+        setIsTopOfPage(false)
+      }
+    }
+    window.addEventListener("scroll",handlescroll);
+    return ()=>window.removeEventListener("scroll",handlescroll);
+  },[])
   
 
   return (
       <div className='app bg-gray-20'>
-     <Navbar />
+     <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Home setSelectedPage={setSelectedPage}/>
       </div>
       
   )
